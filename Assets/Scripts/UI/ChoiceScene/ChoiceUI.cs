98cad0a2876edd501f595b63d1b6350b77b1fea4
Haningyu <haranharan1105@gmail.com>
2025-11-19 21:24:00 +0900

    Add scene movement

--- Assets/Scripts/UI/ChoiceScene/ChoiceUI.cs
+++ Assets/Scripts/UI/ChoiceScene/ChoiceUI.cs
@@ -4,8 +4,20 @@ using UnityEngine.UI;
 
 public class ChoiceUI : MonoBehaviour
 {
-    public void OnClickStart()
+    public void OnClickHAN()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("HAN");
+    }
+    public void OnClickJEONG()
+    {
+        SceneManager.LoadScene("JEONG");
+    }
+    public void OnClickNA()
+    {
+        SceneManager.LoadScene("NA");
+    }
+    public void OnClickSIM()
+    {
+        SceneManager.LoadScene("SIM");
     }
 }
