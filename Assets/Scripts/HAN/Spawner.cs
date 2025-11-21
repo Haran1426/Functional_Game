8ef60fb72f0060c92ba7f2173095dbbd3633e7f5
Haningyu <haranharan1105@gmail.com>
2025-11-21 16:09:34 +0900

    Add probability

--- Assets/Scripts/HAN/Spawner.cs
+++ Assets/Scripts/HAN/Spawner.cs
@@ -3,9 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Rendering;
 
+
+[System.Serializable]
+public class Trash
+{
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    [Range(0f, 50f)]
+    private float chance = 0;
+
+    public float weight { set; get; }
+    public GameObject Prefab => prefab;
+    public float Chance => chance;
+
+}
 public class Spwaner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] prefab;
+    [SerializeField] private Trash[] trashs;
 
     [SerializeField]
     [Range(0, 500)]
@@ -15,22 +30,50 @@ public class Spwaner : MonoBehaviour
     [SerializeField]
     private Vector2 max = new Vector2(8f, 4f);
 
+    private float accumulatedweights;
+
+    private void Awake()
+    {
+        calulateWeights();
+    }
+
     private IEnumerator Start()
     {
         int count = 0;
 
         while (count < maxItemcount)
         {
-            int index = Random.Range(0, prefab.Length);
-            Instantiate(
-                prefab[index],
-                new Vector2(Random.Range(min.x, max.x), Random.Range(min.y, max.y)),
-                Quaternion.identity
-            );
+            SpawnTrash(new Vector2(Random.Range(min.x, max.x), Random.Range(min.y, max.y)));
             count++;
-            Debug.Log(index + "가 생성되었습니다");
 
             yield return new WaitForSeconds(0.01f);
         }
     }
+
+    private void calulateWeights()
+    {
+        accumulatedweights = 0;
+        foreach ( var trash in trashs)
+        {
+            accumulatedweights += trash.Chance;
+            trash.weight = accumulatedweights;
+        }
+
+    }
+    private void SpawnTrash(Vector2 position)
+    {
+        var clone = trashs[GetRandomIndex()];
+        Instantiate(clone.Prefab, position, Quaternion.identity );
+    }
+    private int GetRandomIndex()
+    {   
+        float random = Random.value * accumulatedweights;
+
+        for ( int i = 0; i < trashs.Length; ++i)
+        {
+            if (trashs[i].weight >= random) return i;
+        }
+        return 0;
+    }
+
 }
