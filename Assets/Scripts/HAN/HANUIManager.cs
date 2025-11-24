f57b18214cbabd0a7228991fb890d5ef9b95ffdd
Haningyu <haranharan1105@gmail.com>
2025-11-24 19:38:19 +0900

    Test

--- Assets/Scripts/HAN/HANUIManager.cs
+++ Assets/Scripts/HAN/HANUIManager.cs
@@ -3,13 +3,12 @@ using UnityEngine.UI;
 
 public class HANUIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
